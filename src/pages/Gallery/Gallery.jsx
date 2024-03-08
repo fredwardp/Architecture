3d0f401b160589bc9c7b56115b5eeff682ce15fa
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-03-08 09:31:12 +0100

    gallery img fix

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -7,18 +7,18 @@ const Gallery = () => {
       </h1>
       <section className="photo_section">
         <div className="photo_wrapper">
-          <img src="./public/img/photo1.png" alt="" />
-          <img src="./public/img/photo2.png" alt="" />
-          <img src="./public/img/photo3.png" alt="" />
-          <img src="./public/img/photo4.png" alt="" />
-          <img src="./public/img/photo5.png" alt="" />
+          <img src="/img/photo1.png" alt="" />
+          <img src="/img/photo2.png" alt="" />
+          <img src="/img/photo3.png" alt="" />
+          <img src="/img/photo4.png" alt="" />
+          <img src="/img/photo5.png" alt="" />
         </div>
         <div className="photo_wrapper">
-          <img src="./public/img/photo6.png" alt="" />
-          <img src="./public/img/photo7.png" alt="" />
-          <img src="./public/img/photo8.png" alt="" />
-          <img src="./public/img/photo9.png" alt="" />
-          <img src="./public/img/photo10.png" alt="" />
+          <img src="/img/photo6.png" alt="" />
+          <img src="/img/photo7.png" alt="" />
+          <img src="/img/photo8.png" alt="" />
+          <img src="/img/photo9.png" alt="" />
+          <img src="/img/photo10.png" alt="" />
         </div>
       </section>
     </main>
